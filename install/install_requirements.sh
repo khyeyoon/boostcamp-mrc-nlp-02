ee003132626da85a997f44120f78790dfb1fb36a
AttractiveMinki <wnduq125@naver.com>
2022-05-11 03:35:13 +0000

    fix: add wandb, rank_bm25

--- install/install_requirements.sh
+++ install/install_requirements.sh
@@ -8,6 +8,8 @@ pip install tqdm==4.41.1
 pip install pandas==1.1.4
 pip install scikit-learn==0.24.1
 pip install konlpy==0.5.2
+pip install wandb==0.12.16
+pip install rank_bm25
 
 # faiss install (if you want to)
 pip install faiss-gpu
